6d7d6ec2fd5df40fd040d74253f32e91835ab56d
nickbonanni <nickbo514@yahoo.com>
2021-02-10 12:16:19 -0500

    Added button functionality. Button will change TextView message when clicked for the first time.

--- app/src/main/java/edu/temple/helloworld/MainActivity.java
+++ app/src/main/java/edu/temple/helloworld/MainActivity.java
@@ -1,14 +1,34 @@
 package edu.temple.helloworld;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    TextView helloWorld;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        helloWorld = (TextView) findViewById(R.id.helloWorld);
+        helloWorld.setText(R.string.my_greeting);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                helloWorld.setText(R.string.new_message);
+            }
+        });
     }
+
+
+
 }
